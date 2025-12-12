c29a42ef791fdd2c3442af84751651a8f12a1f35
Jukka Aho <ahojukka5@gmail.com>
2025-12-12 12:08:09 +0200

    test(surface_elements): cover wedge, hex20, and shell mappings
    Add create_surface_element checks for Wedge6/15, Hex20, and shell edge
    extraction to lock in the expanded surface mapping.

--- test/test_create_surface_elements.jl
+++ test/test_create_surface_elements.jl
@@ -21,3 +21,30 @@ end
     @test_throws(Exception, create_surface_element(:Tet5, :S1, [8, 9, 10, 2]))
     @test_throws(Exception, create_surface_element(:Tet4, :S5, [8, 9, 10, 2]))
 end
+
+@testset "surface extraction for wedge, hex20, and shell elements" begin
+    # Wedge6: faces 1..3 are quads, 4..5 are tris
+    wedge_elem = create_surface_element(:Wedge6, :S1, [1, 2, 3, 4, 5, 6])
+    @test wedge_elem == (:Quad4, [1, 4, 5, 2])
+    wedge_tri = create_surface_element(:Wedge6, :S5, [1, 2, 3, 4, 5, 6])
+    @test wedge_tri == (:Tri3, [4, 6, 5])
+
+    # Wedge15: quadratic faces
+    wedge15_quad = create_surface_element(:Wedge15, :S2,
+        [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15])
+    @test wedge15_quad == (:Quad8, [2, 5, 6, 3, 12, 13, 14, 9])
+    wedge15_tri = create_surface_element(:Wedge15, :S4,
+        [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15])
+    @test wedge15_tri == (:Tri6, [1, 2, 3, 8, 9, 7])
+
+    # Hex20: quadratic quads
+    hex20_face = create_surface_element(:Hex20, :S1,
+        collect(1:20))
+    @test hex20_face == (:Quad8, [1, 2, 3, 4, 9, 10, 11, 12])
+
+    # Shells: edge extraction as segments
+    tri6_edge = create_surface_element(:Tri6, :S2, [1, 2, 3, 4, 5, 6])
+    @test tri6_edge == (:Seg3, [2, 3, 5])
+    quad8_edge = create_surface_element(:Quad8, :S4, [1, 2, 3, 4, 5, 6, 7, 8])
+    @test quad8_edge == (:Seg3, [4, 1, 8])
+end
